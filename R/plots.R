43e316fea51b61165909b425cfe1f5b51fb2da9c
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-12-28 15:41:59 +0100

    Add Fig. 1

--- R/plots.R
+++ R/plots.R
@@ -36,3 +36,143 @@ theme_twin <- function(base_size = 11, base_family = "",
     )
 }
 
+
+#' Import plot_grid from the package cowplot
+#'
+#' See [`plot_grid`][`cowplot::plot_grid`] for details.
+#'
+#' @name plot_grid
+#' @export
+#' @importFrom cowplot plot_grid
+NULL
+
+
+#' Import ggsave from the package ggplot2
+#'
+#' See [`ggsave`][`ggplot2::ggsave`] for details.
+#'
+#' @name ggsave
+#' @export
+#' @importFrom ggplot2 ggsave
+NULL
+
+
+#' Functions producing the figures
+#'
+#' @param data a `data.frame` or a `list` containing the data to be plotted
+#' @return a ggplot
+#' @name figures
+#' @examples
+#' # See ?twinR
+#'
+NULL
+
+
+#' @describeIn figures draw fig. 1A
+#' @export
+#'
+draw_fig_1A <- function(data) {
+
+  ## extract results component from the output of compute_predictions() if not done by user
+  if (is.list(data) && !is.data.frame(data)) {
+    data <- data$results
+  }
+
+  data %>%
+    ggplot2::ggplot() +
+    ggplot2::aes(x = factor(.data$twinner),
+                 y = .data$estimate,
+                 ymin = .data$lwr,
+                 ymax = .data$upr) +
+    ggplot2::geom_col(color = "black",
+                      fill = c("lightgray", "white"), width = .6) +
+    ggplot2::geom_errorbar(size = 0.5, width = 0.1, linetype = 1) +
+    ggplot2::labs(y = "Maternal total births", x = "Lifetime twin. status") +
+    ggplot2::scale_x_discrete(labels = c("Non-twinner", "Twinner")) +
+    ggplot2::scale_y_continuous(limits = c(0, 7), breaks = c(0, 2, 4, 6)) +
+    theme_twin()
+}
+
+
+#' @describeIn figures draw fig. 1B
+#' @export
+#'
+draw_fig_1B <- function(data) {
+
+  ## extract results component from the output of compute_predictions() if not done by user
+  if (is.list(data) && !is.data.frame(data)) {
+    data <- data$results
+  }
+
+  data %>%
+    ggplot2::ggplot() +
+    ggplot2::aes(x = .data$births_total,
+                 y = .data$estimate,
+                 ymin = .data$lwr,
+                 ymax = .data$upr,
+                 fill = .data$estimate) +
+    ggplot2::labs(y = "Lifetime twin. prob.",
+                  x = "Maternal total births" ) +
+    ggplot2::geom_line(colour = "black", size = 1) +
+    ggplot2::geom_ribbon(alpha = 0.3, fill = "grey") +
+    ggplot2::scale_x_continuous(breaks = c(1, 5, 10, 15, 18),
+                                labels = c("1", "5", "10", "15", "18")) +
+    ggplot2::expand_limits(y = 0) +
+    theme_twin()
+}
+
+
+#' @describeIn figures draw fig. 1C
+#' @export
+#'
+draw_fig_1C <- function(data) {
+
+  ## extract results component from the output of compute_predictions() if not done by user
+  if (is.list(data) && !is.data.frame(data)) {
+    data <- data$results
+  }
+
+  data %>%
+    ggplot2::ggplot() +
+    ggplot2::aes(x = factor(.data$first_twinner),
+                 y = .data$estimate,
+                 ymin = .data$lwr,
+                 ymax = .data$upr) +
+    ggplot2::geom_col(color = "black", fill = c("lightgray", "white"), width = .6) +
+    ggplot2::geom_errorbar(size = 0.5, width = 0.1, linetype = 1) +
+    ggplot2::labs(y = "Maternal total births", x = "First-birth twin. status") +
+    ggplot2::scale_x_discrete(labels = c("Singleton", "Twins")) +
+    ggplot2::scale_y_continuous(limits = c(0, 7), breaks = c(0, 2, 4, 6)) +
+    theme_twin()
+}
+
+
+#' @describeIn figures draw fig. 1D
+#' @export
+#'
+draw_fig_1D <- function(data) {
+
+  ## extract results component from the output of compute_predictions() if not done by user
+  if (is.list(data) && !is.data.frame(data)) {
+    data <- data$results
+  }
+
+  data %>%
+    ggplot2::ggplot() +
+    ggplot2::aes(x = .data$births_total,
+                 y = .data$estimate,
+                 ymin = .data$lwr,
+                 ymax = .data$upr,
+                 fill = .data$estimate) +
+    ggplot2::labs(y = "First-birth twin. prob.",
+                  x = "Maternal total births" ) +
+    ggplot2::geom_line(colour = "black", size = 1) +
+    ggplot2::geom_ribbon(alpha = 0.3, fill = "grey") +
+    ggplot2::scale_x_continuous(breaks = c(1, 5, 10, 15, 18),
+                                labels = c("1", "5", "10", "15", "18")) +
+    ggplot2::expand_limits(y = 0) +
+    theme_twin()
+}
+
+
+
