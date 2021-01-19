1d54f6098bfc7cef4194da93e50b738c841cef6e
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-01-19 22:15:21 +0100

    Keep bibtex file in source folder

--- R/SI.R
+++ R/SI.R
@@ -61,8 +61,8 @@ build_SI <- function(path_SI = "SI", overwrite = FALSE) {
 
   ## actual job:
   message("the SI is being created (be patient)...")
-  system(paste0("mv ", path_source_SI, "/*.bst ", path_SI))
-  system(paste0("mv ", path_source_SI, "/*.bib ", path_SI))
+  system(paste0("cp ", path_source_SI, "/*.bst ", path_SI))
+  system(paste0("cp ", path_source_SI, "/*.bib ", path_SI))
   vignette_path <- knitr::knit2pdf(input = path_source_SI_file, output = path_tex_file, quiet = FALSE)
   message("the SI should have been created and is stored at the following location:")
   message(vignette_path)
